cb2d832386a55804c007202a7a6511a2bb6d7e5f
Webteroid89 <90185466+Webteroid89@users.noreply.github.com>
2021-11-15 14:37:19 +0300

    Update read.py

--- read.py
+++ read.py
@@ -2,7 +2,7 @@ import csv
 import sys
 filename1 = "first_group.txt"
 filename2 = "second_group.txt"
-fd = open(filename1, "r")
+fd = open(filename2, "r")
 reader = csv.reader(fd, delimiter="\t")
 for row in reader:
     print(row)  
